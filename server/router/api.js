7f9ac1ca54248db04fdcef72af22aa8649f4844d
Jhair Rodrigo Viveros Cordova <cherry199662@gmail.com>
2020-09-19 11:09:50 -0500

    chore: install material & redux

--- server/router/api.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-const { addInvoice, getInvoices } = require("../controllers/invoice");
-
-router.route("/invoice").get(getInvoices);
-router.route("/invoice").post(addInvoice);
-
-module.exports = router;
